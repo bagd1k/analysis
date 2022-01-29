7fd9a980c35e4c5eecf5eefb72ee5ab593cb5572
bagd123 <fcmetalurglp@gmail.com>
2022-01-30 02:08:34 +0300

    Graphs are now fancier Rewrote conclusions.txt

--- main.py
+++ main.py
@@ -42,7 +42,7 @@ def showGraph(xAxis, yAxis, xTitle, yTitle, title='График', width=0.8, siz
     plt.ylabel(yTitle)
     if needsSort:
         dictionary = zipAndSort(xAxis, yAxis)
-        bar = plt.bar(dictionary.keys(), list(map(lambda x: round(x, 4) if type(x) != str else x, dictionary.values())),
+        bar = plt.bar(dictionary.keys(), list(map(lambda x: round(x, 2) if type(x) != str else x, dictionary.values())),
                       width=width)
     else:
         bar = plt.bar(xAxis, yAxis, width=width)
@@ -59,28 +59,31 @@ region = region[['сельсовет' not in name and 'район' not in name f
 districts['Name'] = districts['Name'].apply(lambda x: x.split(' район')[0])  # Слегка обрезаем имена для районов
 populationPercents = makePercentageList(list(districts['Population']))  # Процент населения
 shopsPercent = makePercentageList(list(districts['ShopsAll']))  # Процент магазинов
-coefficient = [i / j for i, j in zip(populationPercents, shopsPercent)]  # Высчитываем коэффициент
+boutiquesPercent = makePercentageList(list(districts['BoutiqueAll']))  # Процент магазинов
+coefficientShops = [i / j for i, j in zip(shopsPercent, populationPercents)]  # Высчитываем коэффициент для магазинов
+coefficientBoutiques = [i / j for i, j in zip(boutiquesPercent, populationPercents)]  # И для киосков
 condition = (region['Delivery'] == 0) & (region['ShopsGoods'] == 0) & (region['BoutiqueGoods'] == 0)
 # Общее условие отсутсвия продуктовых торговых точек
 needsDelivery = region[condition & (region['Population'] <= 100) & (region['Population'] > 0)]
 needsBoutique = region[condition & (region['Population'] > 100) & (region['Population'] < 500)]
 needsShop = region[condition & (region['Population'] > 500)]
-boutiqueDensityByDistricts = districts['BoutiqueAll'] / districts['Population']  # Небольшое исследование плотности
-shopDensityByDistricts = districts['ShopsAll'] / districts['Population']
+boutiqueDensityByDistricts = districts['Population'] / districts['BoutiqueAll']  # Небольшое исследование плотности
+shopDensityByDistricts = districts['Population'] / districts['ShopsAll']
 
-showPieGraph(districts['Population'], districts['Name'], 'Процент населения по районам от общего')
-showPieGraph(districts['BoutiqueAll'], districts['Name'], 'Процент киосков по районам от общего')
-showPieGraph(districts['ShopsAll'], districts['Name'], 'Процент магазинов по районам от общего')
-showGraph(districts['Name'], coefficient,
-          'Районы', 'Отношение кол-ва населения к магазинам', title=f'Среднее: {round(np.mean(coefficient), 2)}')
-showGraph(districts['Name'], districts['ShopsAll'],
-          'Районы', 'Магазины', title=f"Среднее: {round(np.mean(districts['ShopsAll']))}")
-showGraph(districts['Name'], districts['BoutiqueAll'],
-          'Районы', 'Киоски', title=f"Среднее: {round(np.mean(districts['BoutiqueAll']))}")
+showPieGraph(districts['Population'], districts['Name'], 'Процент населения по районам от общего (без г. Липецка)')
+showPieGraph(districts['BoutiqueAll'], districts['Name'], 'Процент киосков по районам от общего (без г. Липецка)')
+showPieGraph(districts['ShopsAll'], districts['Name'], 'Процент магазинов по районам от общего (без г. Липецка)')
+showGraph(districts['Name'], coefficientShops,
+          'Районы', 'Коэффициент процентного отношения магазинов к населению',
+          title=f'Среднее: {round(np.mean(coefficientShops), 2)}')
+showGraph(districts['Name'], coefficientBoutiques,
+          'Районы', 'Коэффициент процентного отношения кисоков к населению',
+          title=f'Среднее: {round(np.mean(coefficientBoutiques), 2)}')
 showGraph(districts['Name'], boutiqueDensityByDistricts, 'Районы', 'Плотность киосков',
-          title=f'Среднее: {round(np.mean(boutiqueDensityByDistricts), 4)}')
+          title=f'Среднее: {round(np.mean(boutiqueDensityByDistricts))}')
 showGraph(districts['Name'], shopDensityByDistricts, 'Районы', 'Плотность магазинов',
-          title=f'Среднее: {round(np.mean(shopDensityByDistricts), 4)}')
+          title=f'Среднее: {round(np.mean(shopDensityByDistricts))}')
 showGraph([0, len(needsShop), len(needsBoutique), len(needsDelivery)],
           ['', 'В магазинах', 'В киосках', 'В приезжающих'], '', 'Тип',
-          title='Количество нуждающихся поселков', width=15, size=10, height=6, showBarLabel=False, needsSort=False)
+          title='Количество нуждающихся населённых пунктов', width=15, size=11, height=6,
+          showBarLabel=False, needsSort=False)
